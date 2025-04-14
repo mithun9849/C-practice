02ca743afa8905f23abafb7f2943c23eaee86ed4
Mithun Kumar <dharur.kumar@s.amity.edu>
2025-04-14 20:29:40 +0530

    String code using 'fgets' instead scanf

---
+++ string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char str[20];
+    printf("Enter ur name:");
+    fgets(str,sizeof(str),stdin);
+    printf("%s",str);
+    
+
+    return 0;
+}
+
+stdin = "standard input"
+str = name of string
